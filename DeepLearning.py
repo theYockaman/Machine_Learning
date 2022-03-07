e7fc1830680c33dab966a0a34e4c07924b9a27d7
Nathan Yockey <93102472+theYockaman@users.noreply.github.com>
2022-03-06 22:12:06 -0600

    New Update
    I noticed a bunch of flaws and adjusted it to work and large amount better.

--- DeepLearning.py
+++ DeepLearning.py
@@ -2,7 +2,7 @@ import numpy as np
 from Functions import Data
 
 class DeepLearning:
-    def __init__(self,Inputs:list,Number_Outputs:int , Target:list = None, Directory:str = "Default_Directory"):
+    def __init__(self,Inputs:list, Number_Outputs:int , Directory:str, Target:list = None):
         self.Inputs = Inputs
         self.Target = Target
         self.Ouputs = []
@@ -62,6 +62,7 @@ class DeepLearning:
         file = Data.JSON_Data(self.Directory)
 
         if file.get("Biases"):
+            
             biases = Data.Convert_Array(file['Biases'])
 
         else:
@@ -74,8 +75,7 @@ class DeepLearning:
                 else:
                     final = self.NueronsPerLayer
 
-                list_biases = np.ones((1, final))
-            
+                list_biases = np.ones((1, final))[0]
                 biases.append(list_biases)
 
         return biases
@@ -83,8 +83,8 @@ class DeepLearning:
     def Intialize_Weight_M(self):
         file = Data.JSON_Data(self.Directory)
 
-        if file.get("Weights M"):
-            m_s = Data.Convert_Array(file['Weights M'])
+        if file.get("Weight M"):
+            m_s = Data.Convert_Array(file['Weight M'])
         else:
             m_s = []
                 
@@ -108,8 +108,8 @@ class DeepLearning:
     def Intialize_Weight_V(self):
         file = Data.JSON_Data(self.Directory)
 
-        if file.get("Weights V"):
-            v_s = Data.Convert_Array(file['Weights V'])
+        if file.get("Weight V"):
+            v_s = Data.Convert_Array(file['Weight V'])
         else:
             v_s = []
                 
@@ -133,46 +133,43 @@ class DeepLearning:
     def Intialize_Bias_M(self):
         file = Data.JSON_Data(self.Directory)
 
-        if file.get("Biased M"):
-            biases = Data.Convert_Array(file['Biases M'])
+        if file.get("Biases M"):
+            print("WHY")
+            v_m = Data.Convert_Array(file["Biases M"])
+
         else:
-            biases = []
+            v_m = []
 
             for layer in range(self.Number_Layers):
 
                 if layer == (self.Number_Layers-1):
                     final = self.Number_Outputs
-
                 else:
                     final = self.NueronsPerLayer
+            
+                layer_v_m = np.zeros((1,final))[0]
+                v_m.append(layer_v_m)
 
-                list_biases = np.zeros((1,final))
-                
-                biases.append(list_biases)
-
-        return biases
+        return v_m
 
     def Intialize_Bias_V(self):
+
         file = Data.JSON_Data(self.Directory)
 
         if file.get("Biases V"):
-            v_s = Data.Convert_Array(file['Biases V'])
+            v_s = Data.Convert_Array(file["Biases V"])
+
         else:
             v_s = []
-                
+
             for layer in range(self.Number_Layers):
 
-                if layer == 0:
-                    intial = self.Number_Inputs 
-                    final = self.NueronsPerLayer
-                elif layer == (self.Number_Layers-1):
-                    intial = self.NueronsPerLayer
+                if layer == (self.Number_Layers-1):
                     final = self.Number_Outputs
                 else:
-                    intial = self.NueronsPerLayer
                     final = self.NueronsPerLayer
-
-                layer_v_s = np.zeros((intial,final))
+            
+                layer_v_s = np.zeros((1,final))[0]
                 v_s.append(layer_v_s)
 
         return v_s
@@ -190,15 +187,14 @@ class DeepLearning:
         return layer
 
     def Update(self):
-        import Programming.Functions as Functions
         dictionary = {}
 
-        dictionary.update({"Weights":Data.Convert_List(self.Weights)})
-        dictionary.update({"Biases":Data.Convert_List(self.Biases)})
-        dictionary.update({"Weight M":Data.Convert_List(self.Weight_Prev_M)})
-        dictionary.update({"Weight V":Data.Convert_List(self.Weight_Prev_V)})
-        dictionary.update({"Biases M":Data.Convert_List(self.Bias_Prev_M)})
-        dictionary.update({"Biases V":Data.Convert_List(self.Bias_Prev_V)})
+        dictionary.update({"Weights":self.Weights})
+        dictionary.update({"Biases":self.Biases})
+        dictionary.update({"Weight M":self.Weight_Prev_M})
+        dictionary.update({"Weight V":self.Weight_Prev_V})
+        dictionary.update({"Biases M":self.Bias_Prev_M})
+        dictionary.update({"Biases V":self.Bias_Prev_V})
 
         Data.JSON_Dump(dictionary,self.Directory)
 
@@ -214,6 +210,7 @@ class DeepLearning:
             for layer_weights in reverse_weights:
                 layer_errors = []
                 weight_sums = []
+                weight_sums = [np.sum(o) for o in layer_weights.T]
                 
                 for o in layer_weights.T:
                     weight_sums.append(np.sum(o))
@@ -238,7 +235,6 @@ class DeepLearning:
             return new_errors
         
         def New_Weight( Old_Weight, Error, Hidden_Layer, Prev_V, Prev_M):
-        
             Error = Error * Hidden_Layer
             m = self.B1 * Prev_M + (1-self.B1) * Error
             v = self.B2 * Prev_V + (1-self.B2) * Error **2
@@ -253,92 +249,98 @@ class DeepLearning:
 
         def New_Bias( Old_Bias, Error, Prev_V, Prev_M):
         
-            m = self.B1 * Prev_M + (1-self.B1) * Error
-            v = self.B2 * Prev_V + (1-self.B2) * Error **2
+            m = (self.B1 * Prev_M + (1-self.B1) * Error)
+            v = (self.B2 * Prev_V + (1-self.B2) * Error **2)
             e = 1 ** -8
             
             hm = m/(1-self.B1)
             hv = v/(1-self.B2)
 
-            new_B = Old_Bias - (hm * self.A)/ (hv**0.5 + e) 
-            
-            return new_B, m, v
+            new_B = (Old_Bias - (hm * self.A)/ (hv**0.5 + e))
+            new_V = v
+            new_M = m
+            return new_B, new_M, new_V
 
-        self.Errors =  Calculate_dErrors() 
-        new_biases = []
-        new_weights = []
+        def Calculate_New_Weights():
+            # Retrieve Variables or Nessary Material
+            weights = self.Weights[::-1]
+            nuerons = self.Nuerons[::-1]
+            errors = self.Errors[::-1]
+            prev_wv = self.Weight_Prev_V[::-1]
+            prev_wm = self.Weight_Prev_M[::-1]
 
-        new_bm = []
-        new_bv = []
+            new_weights = []
+            new_wm = []
+            new_wv = []
 
-        new_wm = []
-        new_wv = []
-        
-        # Reverse Biases, Weights, Errors, and Nuerons
-        reverse_biases = self.Biases[::-1]
-        reverse_weights = self.Weights[::-1]
-        reverse_errors = self.Errors[::-1]
-        reverse_nuerons = self.Nuerons[::-1]
-        reverse_bv = self.Bias_Prev_V[::-1]
-        reverse_bm = self.Bias_Prev_M[::-1]
-        reverse_wv = self.Weight_Prev_V[::-1]
-        reverse_wm = self.Weight_Prev_M[::-1]
+            for layer_weights, layer_wv, layer_wm, layer_errors, x in zip(weights,prev_wv, prev_wm, errors, range(self.Number_Layers)):
+                new_layer_weights = []
+                new_layer_wv = []
+                new_layer_wm = []
 
-        # Calculate New Biases
-        for x in range(self.Number_Layers):
-            layer_biases = []
-            layer_v = []
-            layer_m = []
-            
-            for bias, error, v, m, y in zip(reverse_biases[x],reverse_errors[x], reverse_bv[x], reverse_bm[x], range(len(reverse_errors[x]))):
+                for nueron_weights, nueron_wv, nueron_wm, hidden_layer in zip(layer_weights,layer_wv,layer_wm, nuerons[x+1][0]):
+                    new_neuron_weights = []
+                    neuron_wv = []
+                    neuron_wm = []
 
-                new_bias, new_m, new_v = New_Bias(bias,error,v,m)
+                    for weight, wv, wm, w_error in zip(nueron_weights,nueron_wv,nueron_wm,layer_errors):
 
-                layer_v.append(new_v)
-                layer_m.append(new_m)
-                
-                layer_biases.append(new_bias)
+                        new_weight, n_wm, n_wv = New_Weight(weight,w_error,hidden_layer,wv,wm)
 
-            new_biases.append(np.array(layer_biases))
-            new_bv.append(np.array(layer_v))
-            new_bm.append(np.array(layer_m))
-
-        # Calculate New Weights
-        for layer_weights, x in zip(reverse_weights,range(self.Number_Layers)):
-            layer_new_weights = []
-            layer_v = []
-            layer_m = []
-               
-            for neuron_weights, neuron_wv, neuron_wm, hl in zip(layer_weights,reverse_wv[x], reverse_wm[x], reverse_nuerons[x+1][0]):
-                new_neuron_weights = []
-                neuron_v = []
-                neuron_m = []
-                
-                for weight, error, v, m in zip(neuron_weights,reverse_errors[x],neuron_wv, neuron_wm):
+                        neuron_wv.append(n_wv.tolist())
+                        neuron_wm.append(n_wm.tolist())
+                            
+                        new_neuron_weights.append(new_weight)
                     
-                    new_weight, new_m, new_v = New_Weight(weight,error,hl,v,m)
 
-                    neuron_v.append(new_v)
-                    neuron_m.append(new_m)
+                    new_layer_wv.append(neuron_wv)
+                    new_layer_wm.append(neuron_wm)
+                    new_layer_weights.append(new_neuron_weights)
+
+                new_weights.append(new_layer_weights)
+                new_wv.append(new_layer_wv)
+                new_wm.append(new_layer_wm)
+
+            return new_weights[::-1], new_wm[::-1], new_wv[::-1]
+
+        def Calculate_New_Biases():
+            # Retrieve Variables or Nessary Material
+            biases = self.Biases[::-1]
+            errors = self.Errors[::-1]
+            prev_bv = self.Bias_Prev_V[::-1]
+            prev_bm = self.Bias_Prev_M[::-1]
+
+            new_biases = []
+            new_bm = []
+            new_bv = []
+
+            for layer_biases, layer_bv, layer_bm, layer_errors in zip(biases,prev_bv,prev_bm,errors):
+                new_layer_biases = []
+                new_layer_bm = []
+                new_layer_bv = []
+
+                for bias, bv, bm, b_error in zip(layer_biases,layer_bv,layer_bm,layer_errors):
                     
-                    new_neuron_weights.append(new_weight)
+                    new_bias, n_bm, n_bv = New_Bias(bias,b_error,bv,bm)
+                    
+                    new_layer_biases.append(new_bias)
+                    new_layer_bv.append(n_bv)
+                    new_layer_bm.append(n_bm)
 
-                layer_v.append(neuron_v)
-                layer_m.append(neuron_m)
-                layer_new_weights.append(new_neuron_weights)
+                new_biases.append(new_layer_biases)
+                new_bv.append(new_layer_bv)
+                new_bm.append(new_layer_bm)
 
-            new_weights.append(np.array(layer_new_weights))
-            new_wv.append(np.array(layer_v))
-            new_wm.append(np.array(layer_m))
+            
+            return new_biases[::-1], new_bm[::-1], new_bv[::-1]
 
-        # Reversing the New Weights & Biases
-        self.Weights = new_weights[::-1]
-        self.Biases = new_biases[::-1]
-        self.Bias_Prev_M = new_bm[::-1]
-        self.Bias_Prev_V = new_bv[::-1]
-        self.Weight_Prev_M = new_wm[::-1]
-        self.Weight_Prev_V = new_wv[::-1]
-                  
+        # Intialize Variables
+        self.Errors = Calculate_dErrors()
+
+        self.Weights,self.Weight_Prev_M,self.Weight_Prev_V = Calculate_New_Weights()
+
+        self.Biases,self.Bias_Prev_M,self.Bias_Prev_V = Calculate_New_Biases()
+    
     def Forward_Propagation(self):
         for x in range(self.Number_Layers):
             # Layer
@@ -371,7 +373,6 @@ class DeepLearning:
             self.Nuerons.append(hidden_layer)
 
     def Back_Propagation(self):
-        
         self.Calculate_New()
         self.Update()     
 
@@ -381,4 +382,5 @@ class DeepLearning:
 
     def Anwser(self):
         self.Forward_Propagation()
+
 
